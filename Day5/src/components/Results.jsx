41edf7ca32aea1fcbc450b8049a6594e411c94df
Shafat Alam <Shafat.mahtab@gmail.com>
2024-03-02 14:58:11 +0600

    Update Results.jsx

--- Day5/src/components/Results.jsx
+++ Day5/src/components/Results.jsx
@@ -1,9 +1,24 @@
-import { calculateInvestmentResults } from '../util/investment.js';
+import { calculateInvestmentResults, formatter } from '../util/investment.js';
 
 export default function Results({ input }) {
     const resultsData = calculateInvestmentResults(input);
+    const initialInvestment =
+        resultsData[0].valueEndOfYear -
+        resultsData[0].interest -
+        resultsData[0].annualInvestment;
 
-    console.log(resultsData);
-
-    return <p>Results...</p>
+    return (
+        <table id="result">
+        <thead>
+            <tr>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+            </tr>
+        </thead>
+        
+        </table>
+    );
 }
