fd7705413b510f9ee3c966043835c2aebb9da97d
Shafat Alam <Shafat.mahtab@gmail.com>
2024-03-02 14:58:17 +0600

    Update Results.jsx

--- Day5/src/components/Results.jsx
+++ Day5/src/components/Results.jsx
@@ -18,7 +18,25 @@ export default function Results({ input }) {
             <th>Invested Capital</th>
             </tr>
         </thead>
-        
+        <tbody>
+            {resultsData.map((yearData) => {
+            const totalInterest =
+                yearData.valueEndOfYear -
+                yearData.annualInvestment * yearData.year -
+                initialInvestment;
+            const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+
+            return (
+                <tr key={yearData.year}>
+                <td>{yearData.year}</td>
+                <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                <td>{formatter.format(yearData.interest)}</td>
+                <td>{formatter.format(totalInterest)}</td>
+                <td>{formatter.format(totalAmountInvested)}</td>
+                </tr>
+            );
+            })}
+        </tbody>
         </table>
     );
 }
